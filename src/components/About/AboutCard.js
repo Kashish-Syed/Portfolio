d5c1e8064e3075f0a416145b931d2e074c8a5351
Kashish Syed <ksyed2@huskers.unl.edu>
2025-05-03 23:33:36 -0700

    Added personalization for the home page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Kashish Syed </span>
+            from <span className="purple"> Lincoln, Nebraska.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I'm currently pursuing a Bachelor's degree in Computer Science from the University of Nebraska - Lincoln.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am actively seeking Software Engineering Internship opportunities.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -32,9 +31,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          "Code with purpose, design with passion, and always stay curious."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Kashish</footer>
         </blockquote>
       </Card.Body>
     </Card>
